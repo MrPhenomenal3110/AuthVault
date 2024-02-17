e0682920c1f85b6b5f62869a7ba69b03ff7a23fb
Prem B. Shah <techie.prembshah@gmail.com>
2024-02-17 20:01:44 +0530

    Used HashRouter instead of BrowserRouter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Signup from './Signup';
 import Login from './Login';
 import Dashboard from './Dashboard';
